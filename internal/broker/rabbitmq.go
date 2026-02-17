2bcfc4bbe11c673670201e301d2598f8885841eb
arsagyr <sdkalsky@gmail.com>
2026-02-17 19:21:14 +0700

    Аан судургу олого

--- internal/broker/rabbitmq.go
+++ internal/broker/rabbitmq.go
@@ -1,1 +1,94 @@
 package broker
+
+import (
+	"encoding/json"
+
+	"github.com/streadway/amqp"
+)
+
+type RabbitMQClient struct {
+	conn    *amqp.Connection
+	channel *amqp.Channel
+	config  *amqp.Config
+}
+
+func NewRabbitMQClient(url string) (*RabbitMQClient, error) {
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		return nil, err
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+
+	// Настраиваем QoS
+	err = ch.Qos(
+		1,     // prefetch count
+		0,     // prefetch size
+		false, // global
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &RabbitMQClient{
+		conn:    conn,
+		channel: ch,
+	}, nil
+}
+
+func (c *RabbitMQClient) DeclareQueue(name string) error {
+	_, err := c.channel.QueueDeclare(
+		name,
+		true,  // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	return err
+}
+
+func (c *RabbitMQClient) PublishMessage(queue string, body interface{}) error {
+	data, err := json.Marshal(body)
+	if err != nil {
+		return err
+	}
+
+	err = c.channel.Publish(
+		"",    // exchange
+		queue, // routing key
+		false, // mandatory
+		false, // immediate
+		amqp.Publishing{
+			ContentType:  "application/json",
+			Body:         data,
+			DeliveryMode: amqp.Persistent, // делаем сообщения персистентными
+		})
+
+	return err
+}
+
+func (c *RabbitMQClient) ConsumeMessages(queue string) (<-chan amqp.Delivery, error) {
+	msgs, err := c.channel.Consume(
+		queue,
+		"",    // consumer
+		false, // auto-ack (false для ручного подтверждения)
+		false, // exclusive
+		false, // no-local
+		false, // no-wait
+		nil,
+	)
+	return msgs, err
+}
+
+func (c *RabbitMQClient) Close() {
+	if c.channel != nil {
+		c.channel.Close()
+	}
+	if c.conn != nil {
+		c.conn.Close()
+	}
+}
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,25 @@
 package config
+
+import "os"
+
+type Config struct {
+	RabbitMQURL string
+	Port        string
+}
+
+func Load() *Config {
+	rabbitURL := os.Getenv("RABBITMQ_URL")
+	if rabbitURL == "" {
+		rabbitURL = "amqp://guest:guest@localhost:5672/"
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	return &Config{
+		RabbitMQURL: rabbitURL,
+		Port:        port,
+	}
+}
